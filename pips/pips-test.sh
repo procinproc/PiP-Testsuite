3df5c3344e194fbc96fdfcb6af6ff65ec3df2abc
ahori@riken.jp <ahori@riken.jp>
2021-03-04 12:10:05 +0900

    some minor changes

--- pips/pips-test.sh
+++ pips/pips-test.sh
@@ -85,6 +85,8 @@ do_test() {
     shift
     pips_file=$1
     shift
+    unset PIP_CHECK_GEN
+    unset PIP_CHECK_OUT
     if $flag_gen; then
 	echo -n "pips $@"
 	echo "pips $@" >> $pips_log
@@ -106,20 +108,27 @@ do_test() {
 	echo ">>>> pips $@" >> $pips_log
 	nofile=false
 	if ! [ -f ${pips_file}.input ]; then
-	    nofile=true
 	    flag_error=true
+	    nofile=true
 	    echo "${pips_file}.input is missing"
 	    echo "${pips_file}.input is missing" >> $pips_log
 	fi
 	if ! [ -f ${pips_file}.output ]; then
-	    nofile=true
 	    flag_error=true
+	    nofile=true
 	    echo "${pips_file}.output is missing"
 	    echo "${pips_file}.output is missing" >> $pips_log
 	fi
 	if ! $nofile; then
 	    PIPS_CHECK_OUT=${pips_file} ${PIPS} $@ >> $pips_log 2>&1
-	    check $flag $? ${pips_file}
+	    extval=$?
+	    if ! [ -f ${pips_file}.check ]; then
+		flag_error=true
+		echo "${pips_file}.check is missing"
+		echo "${pips_file}.check is missing" >> $pips_log
+	    else
+		check $flag $extval ${pips_file}
+	    fi
 	fi
     fi
 }
@@ -135,39 +144,39 @@ fi
 
 do_test true  pips-noop
 do_test true  pips-noop-  -
-do_test true  pips-noop-a - a
+do_test true  pips-noop-a - ~a~
 do_test true  pips-noop--  --
-do_test true  pips-noop--b - b
+do_test true  pips-noop--b - ~b~
 
-do_test true  pips-a    a a
+do_test true  pips-a    a ~a~
 do_test true  pips-u    u
 do_test true  pips-x    x
 do_test false pips-x-nosuch x nosuchpip
-do_test true  pips-au   au a b
+do_test true  pips-au   au ~a~ ~b~
 do_test true  pips-ax   ax
 do_test true  pips-ua   ua
 do_test true  pips-ux   ux
 do_test true  pips-xa   xa
 do_test true  pips-xu   xu
-do_test true  pips-aux  aux a b c
+do_test true  pips-aux  aux ~a~ ~b~ ~c~ ~d~
 
 do_test true  pips-root au --root
 do_test true  pips-r       -r
 do_test true  pips-r-exec  -r pip-exec
-do_test false pips-r-a     -r a
+do_test false pips-r-a     -r ~a~
 do_test true  pips-task    --task
 do_test true  pips-au-t    au -t
 do_test false pips-t-exec  -t pip-exec
 
 do_test true  pips-family   --family
 do_test true  pips-f        -f
-do_test true  pips-family-a --family a
-do_test true  pips-f-a-c    -f a c
+do_test true  pips-family-a --family ~a~
+do_test true  pips-f-a-c    -f ~a~ ~c~
 do_test false pips-f-nosuch -f nosuchpip
 
 do_test true  pips-test-kill      --kill
 do_test true  pips-task-k        --task -k
-do_test true  pips-task-k-a      --task -k a
+do_test true  pips-task-k-a      --task -k ~a~
 do_test false pips-signal        --signal
 do_test false pips-r-s           -r -s
 do_test true  pips-s-hup         -s hup
@@ -180,15 +189,16 @@ do_test false pips-s-nosuch      -s nosuchsig
 do_test true  pips-ps        --ps
 do_test false pips-ps-nosuch --ps - nosuchpip
 do_test true  pips-P         -P
-do_test true  pips-ps-a      --ps -- a
-do_test true  pips-P-a-c     -P - a c
+do_test true  pips-ps-a      --ps -- ~a~
+do_test true  pips-P-a-c     -P - ~a~ ~c~
 do_test true  pips-ps-l      --ps l
 do_test true  pips-P-l       -P l
-do_test true  pips-ps-l-a    --ps l - a
-do_test true  pips-P-l-b-c   -P l -- b c
+do_test true  pips-ps-l-a    --ps l - ~a~
+do_test true  pips-P-l-b-c   -P l -- ~b~ ~c~
 
 if $flag_error; then
     echo "FAILED"
     exit 1
 fi
+echo "succeeded"
 exit 0
