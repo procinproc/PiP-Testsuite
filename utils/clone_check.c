b037862a952ffd93e4004d8f790c3a339cd923c8
Atsushi Hori <ahori@riken.jp>
2021-05-28 09:15:23 +0900

    use <pip/pip_clone.h> for the clone flag settings

--- utils/clone_check.c
+++ utils/clone_check.c
@@ -1,22 +1,26 @@
-#define _GNU_SOURCE                                                                    
+#define _GNU_SOURCE
 #include <sched.h>
 #include <stdio.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <errno.h>
+#include <pip/pip_clone.h>
 
 int foo( void *arg ) { return 0; }
 
 char stack[8192];
 
 main() {
-  int flags = CLONE_VM;
+  int flags = pip_clone_flags( 0 );
   pid_t pid;
+
+  flags &= ~(CLONE_SETTLS); 	/* 0x80000 */
   if( ( pid = clone( foo, stack, flags, NULL ) ) < 0 ) {
     return 1;
   }
   while( 1 ) {
+    errno = 0;
     if( wait( NULL ) == pid || errno != EINTR ) break;
   }
-  return 0;
+  return errno;
 }
